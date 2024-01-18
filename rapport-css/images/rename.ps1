a162037808129cc96b9af38b4432be11d7c19f6e
amine.lamchatab <alamchatab@gmail.com>
2024-01-18 14:51:15 +0100

    save

---
+++ rapport-css/images/rename.ps1
@@ -0,0 +1,8 @@
+
+
+
+
+rename-item ".\Diapositive1.PNG" "introduction.png"
+rename-item ".\Diapositive2.PNG" "carte-empathie-entreiten-social.png"
+rename-item ".\Diapositive3.PNG" "carte-empathie-medecin-general.png"
+rename-item ".\Diapositive4.PNG" "carte-empathie-d'orthoptiste.png"
