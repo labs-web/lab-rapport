253433effa251063b10a13ebd794cbc129ef5a62
Hamid Achaou <hamidAchaou379@gmail.com>
2024-02-14 11:07:39 +0100

    create table mtere

---
+++ docs/0-table-matiere/table-matier.md
@@ -0,0 +1,9 @@
+---
+layout: default
+order: 1
+slug: "aa"
+---
+
+# table matière 
+
+<!-- new slide -->
---
+++ docs/assets/js/table-matier.js
@@ -0,0 +1,28 @@
+// Select the table-de-matier element, select all h1 elements, and iterate over each h1 element
+const tableDeMatier = document.getElementById('table-matière');
+const h1Elements = document.querySelectorAll('h1');
+// const h2Elements = document.querySelectorAll('h2');
+
+// Create a new unordered list element
+const ul = document.createElement('ul');
+
+h1Elements.forEach(function(h1,h2) {
+    // Create a new list item element
+    const li = document.createElement('li');
+
+    // Create a new anchor element, set its href attribute to the corresponding h1's id, set its text content to the h1's text content, and insert the new anchor into the list item
+    const newAnchor = document.createElement('a');
+    newAnchor.href = "#" + h1.id;
+    newAnchor.textContent = h1.textContent;
+    newAnchor.style.fontWeight = "bold"; // Adding bold font style
+
+    // Append the anchor element to the list item
+    li.appendChild(newAnchor);
+
+    // Append the list item to the unordered list
+    ul.appendChild(li);
+});
+
+// Insert the unordered list after the table-de-matier element
+tableDeMatier.insertAdjacentElement('afterend', ul);
+
